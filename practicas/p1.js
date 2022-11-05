5bb06c8af2e1ff87b8620f759ca5f5f32181f5d7
noemisael <noemisaelantoniofigueroa@gmail.com>
2022-11-05 16:18:32 -0600

    update2.0

---
+++ practicas/p1.js
@@ -0,0 +1,18 @@
+var c=0;
+var num;
+function contar(){
+  num= document.getElementById('tContador').value;
+  num=parseInt(num);
+  c = c+num;
+  c = parseInt(c);
+  document.getElementById('tContador').value = c;
+}
+
+
+
+
+
+
+
+
+
